3642852c78e472c2959c5f4099d354307017da69
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-04-03 07:00:50 +0200

    buildSetup.sh: Add comment

--- build-tools/buildSetup.sh
+++ build-tools/buildSetup.sh
@@ -3,6 +3,7 @@
 
 #Supported devices:
 declare -a DEV_LIST=("pixi-cdl200" "pixi-cq100" "pixi-cq200" "crix-cq100" "crix-arq100" "blix-bdl100" "blix-bq100" "4cbox-cdl100" "4cbox-cdl200" "4cbox-cq100" "4cbox-cq200")
+#Supported distributions:
 declare -a DIST_LIST=("ces-fb" "ces-x11")
 DEVICE=$1
 DIST=$2
