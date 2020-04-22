2e4165db4f43cb9edc8c0e034d3fadb7454f3c4d
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-04-22 16:02:35 +0200

    installRepo.sh: Fix if condition syntax

--- installRepo.sh
+++ installRepo.sh
@@ -2,7 +2,7 @@
 REPO_SOURCE="http://commondatastorage.googleapis.com/git-repo-downloads/repo"
 HOME_PATH=$1
 
-if [ ! -d "${HOME_PATH}/bin" ]
+if [ ! -d "${HOME_PATH}/bin" ]; then
   mkdir ${HOME_PATH}/bin
   curl ${REPO_SOURCE} > ${HOME_PATH}/bin/repo
   chmod a+x ${HOME_PATH}/bin/repo
