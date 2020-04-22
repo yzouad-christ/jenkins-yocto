1ce7107df6d5c49c9258600ca7da043e0d720aa2
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-04-22 12:07:47 +0200

    Edit PATH variable manually

--- execBitbake.sh
+++ execBitbake.sh
@@ -9,4 +9,5 @@ DISTRO=ces-fb
 source ./setup-environment build/ | echo 'y'
 cd ${HOME_PATH}/ces-bsp-platform/build
 touch ${HOME_PATH}/ces-bsp-platform/build/conf/sanity.conf
+PATH=/home/jenkins/ces-bsp-platform/sources/poky/scripts:/home/jenkins/ces-bsp-platform/sources/poky/bitbake/bin:${PATH}
 ${HOME_PATH}/ces-bsp-platform/sources/poky/bitbake/bin/bitbake -D -v ces-qt-demoimage
