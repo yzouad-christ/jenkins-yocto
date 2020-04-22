2bd2adc163473d5197547e1f1eba25f1d65e7efc
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-04-22 16:21:01 +0200

    Comment repo sync command

--- getRepos.sh
+++ getRepos.sh
@@ -10,6 +10,6 @@ fi
 cd $HOME_PATH/ces-bsp-platform
 
 if [ ! "$(ls -A ${HOME_PATH}/ces-bsp-platform)" ]; then
-$HOME_PATH/bin/repo init -u https://github.com/software-celo/ces-bsp-platform -b $BRANCH
-$HOME_PATH/bin/repo sync
+  $HOME_PATH/bin/repo init -u https://github.com/software-celo/ces-bsp-platform -b $BRANCH
+  #$HOME_PATH/bin/repo sync
 fi
