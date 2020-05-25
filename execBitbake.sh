f5370fbfb736e3532ee0c584e797acf94eca9f1f
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-05-25 14:15:22 +0200

    Update execBitbake.sh

--- execBitbake.sh
+++ execBitbake.sh
@@ -57,7 +57,7 @@ source ./setup-environment build/ | echo 'y'
 cd ${HOME_PATH}/ces-bsp-platform/build
 
 # copy new bblayer.conf if customer image has to be build
-if [ ! -z BRANCH_ARG ];
+if [ ! -z BRANCH_ARG ]; then
   cp ${HOME_PATH}/bblayers.conf ${HOME_PATH}/ces-bsp-platform/build/conf
 fi
 
