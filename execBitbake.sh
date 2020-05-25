97e143a11532fe5fd425f1cbcba546535157eb34
Yassine Zouad <yzouad@christ-es.de>
2020-05-25 10:37:00 +0200

    execBitbake.sh: Add branch select for meta-customer

--- execBitbake.sh
+++ execBitbake.sh
@@ -4,6 +4,7 @@ HOME_PATH=$1
 MACHINE_ARG=$2
 DISTRO_ARG=$3
 IMAGE_ARG=$4
+BRANCH_ARG=$5
 
 #Supported devices:
 declare -a DEV_LIST=("pixi-cdl200" "pixi-cq100" "pixi-cq200" "crix-cq100" "crix-arq100" "blix-bdl100" "blix-bq100" "4cbox-cdl100" "4cbox-cdl200" "4cbox-cq100" "4cbox-cq200")
@@ -12,6 +13,15 @@ declare -a DIST_LIST=("ces-fb" "ces-x11")
 
 cd ${HOME_PATH}/ces-bsp-platform
 
+# checkout branch in meta-customer
+if [ ! -z $BRANCH_ARG  ]; then
+  BRANCH=$BRANCH_ARG
+  cd sources/meta-customer
+  git checkout $BRANCH
+  cd ${HOME_PATH}/ces-bsp-platform
+fi
+
+# check if arguments are set
 DEFAULT=0
 if [ ! -z $MACHINE_ARG ]; then
   MACHINE=${MACHINE_ARG}
@@ -30,6 +40,7 @@ else
   DEFAULT=1
 fi
 
+# set default mode if DEFAULT variable is set
 if [ $DEFAULT -eq 1 ]; then
   echo "WARNING: Build configurations not set"
   echo "WARNING: Setting default configuration"
@@ -43,15 +54,10 @@ fi
 # source setup-environment
 source ./setup-environment build/ | echo 'y'
 
-# Create sanity.conf to be able to build as root
 cd ${HOME_PATH}/ces-bsp-platform/build
-#touch ${HOME_PATH}/ces-bsp-platform/build/conf/sanity.conf
 
 # Accept FSL_EULA
 echo "ACCEPT_FSL_EULA = \"1\"" >> ${HOME_PATH}/ces-bsp-platform/build/conf/local.conf
-
-# Set this variable to build as root
-#FORCE_UNSAFE_CONFIGURE=1
 
 # Expand PATH variable
 PATH=/home/jenkins/ces-bsp-platform/sources/poky/scripts:/home/jenkins/ces-bsp-platform/sources/poky/bitbake/bin:${PATH}
