2e7179f99d2a893eb2dfc221ef5cb5d84b84ade5
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-05-27 13:11:11 +0200

    Update getRepos.sh

--- getRepos.sh
+++ getRepos.sh
@@ -2,6 +2,7 @@
 
 HOME_PATH=$1
 BRANCH=$2
+GIT_URL=$3
 
 if [ ! -d "${HOME_PATH}/ces-bsp-platform" ]; then
   mkdir $HOME_PATH/ces-bsp-platform
@@ -10,5 +11,5 @@ fi
 cd $HOME_PATH/ces-bsp-platform
 
 if [ ! "$(ls -A ${HOME_PATH}/ces-bsp-platform)" ]; then
-  $HOME_PATH/bin/repo init -u https://github.com/software-celo/ces-bsp-platform -b $BRANCH
+  $HOME_PATH/bin/repo init -u $GIT_URL -b $BRANCH
 fi
