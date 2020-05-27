8dfe1946d16f3d4eea830feffdcef4232ac7cff4
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-05-27 13:53:28 +0200

    Update getRepos.sh

--- getRepos.sh
+++ getRepos.sh
@@ -10,6 +10,14 @@ fi
 
 cd $HOME_PATH/ces-bsp-platform
 
-#if [ ! "$(ls -A ${HOME_PATH}/ces-bsp-platform)" ]; then
+if [ ! "$(ls -A ${HOME_PATH}/ces-bsp-platform)" ]; then
   $HOME_PATH/bin/repo init -u $GIT_URL -b $BRANCH
-#fi
+else
+  cd /tmp
+  mkdir /tmp/ces-bsp-platform
+  $HOME_PATH/bin/repo init -u $GIT_URL -b $BRANCH
+  $HOME_PATH/bin/repo sync
+  cd /tmp/ces-bsp-platform
+  cp -r /tmp/ces-bsp-platform/sources /home/jenkins/ces-bsp-platform
+  rm -rf /tmp/ces-bsp-platform
+fi
