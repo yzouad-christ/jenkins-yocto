f5c10dc2a84a34582a86f516f545b6506cd17563
yzouad-christ <61199220+yzouad-christ@users.noreply.github.com>
2020-06-10 12:35:41 +0200

    Update getRepos.sh

--- getRepos.sh
+++ getRepos.sh
@@ -12,11 +12,13 @@ cd $HOME_PATH/ces-bsp-platform
 
 if [ ! "$(ls -A ${HOME_PATH}/ces-bsp-platform)" ]; then
   $HOME_PATH/bin/repo init -u $GIT_URL -b $BRANCH
+  sed -i 's#https://github.com/software-celo#ssh://git@gitlab.christ-ag.com:/es#g' .repo/manifests/default.xml
   $HOME_PATH/bin/repo sync
 else
   mkdir /tmp/ces-bsp-platform
   cd /tmp/ces-bsp-platform
   $HOME_PATH/bin/repo init -u $GIT_URL -b $BRANCH
+  sed -i 's#https://github.com/software-celo#ssh://git@gitlab.christ-ag.com:/es#g' .repo/manifests/default.xml
   $HOME_PATH/bin/repo sync
   cp -r /tmp/ces-bsp-platform/sources /home/jenkins/ces-bsp-platform
   rm -rf /tmp/ces-bsp-platform
